817f1a2fadd6a40d7f56e36d1464dc6fa1e362d1
Dtan713 <s.donaldtan@gmail.com>
2024-07-08 12:58:36 -0400

    part9

---
+++ src/PA30371part9.java
@@ -0,0 +1,9 @@
+public class PA30371part9 {
+    public static void main(String[] args) {
+        int[] Arr = {4, 2, 9, 13, 1, 0};
+        Array.sort(Arr);
+        System.out.println("Assending order:"+ Array.toString(Arr));
+        System.out.println("Smallest number is:" + Arr[0]);
+        System.out.println("biggest number is:"+ Arr[5]));
+    }
+}
