de4c6659c5199aab8e3474b94c2ba116c3aa7b72
Dtan713 <s.donaldtan@gmail.com>
2024-07-08 11:19:23 -0400

    part 1 and 2

---
+++ src/PA30371part2.java
@@ -0,0 +1,11 @@
+public class PA30371part2 {
+    public static void main(String [] args) {
+
+        int[] array ={13, 5, 7, 68, 2};
+        int element = array[(array.length)/2];
+        System.out.println(element);
+
+
+
+    }
+}
